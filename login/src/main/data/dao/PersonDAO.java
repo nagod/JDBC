1b89c23ae382455a47a44866f3be6e651fde29a7
nagod <deniz-dogan@t-online.de>
2021-04-29 12:16:05 +0200

    gitignore test

--- login/src/main/data/dao/PersonDAO.java
+++
@@ -1,9 +0,0 @@
-package main.data.dao;
-
-import main.bdo.Person;
-import java.util.List;
-public interface PersonDAO {
-    List<Person> getAll();
-    List<Person> delete(Person person);
-    void createPerson(String first_name, String last_name, String city, String favorite_color);
-}
--- searchPerson/src/main/java/data/dao/JobHistoryDAO.java
+++
@@ -1,6 +0,0 @@
-package main.java.data.dao;
-
-
-
-public interface JobHistoryDAO {
-}
